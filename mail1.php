ecb694811ea1cd0ea82b40a14ecccd095395de31
ThyagoAlves93 <72070438+ThyagoAlves93@users.noreply.github.com>
2020-10-30 12:03:18 +1100

    Add file CA2018400

---
+++ mail1.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'functions.php';
+
+if (!empty($_POST)){
+
+  $data['success'] = true;
+  $_POST  = multiDimensionalArrayMap('cleanEvilTags', $_POST);
+  $_POST  = multiDimensionalArrayMap('cleanData', $_POST);
+
+  $emailTo ="othyagogames@thyagogames.com"; //"othyagogames@thyagogames.com";
+
+
+  $emailFrom ="contact@thyagogames.com"; //"contact@thyagogames.com";
+
+
+  $emailSubject = "Mail from Porta";
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $comment = $_POST["comment"];
+  if($name == "")
+   $data['success'] = false;
+ 
+ if (!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email)) 
+   $data['success'] = false;
+
+
+ if($comment == "")
+   $data['success'] = false;
+
+ if($data['success'] == true){
+
+  $message = "NAME: $name<br>
+  EMAIL: $email<br>
+  COMMENT: $comment";
+
+
+  $headers = "MIME-Version: 1.0" . "\r\n"; 
+  $headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
+  $headers .= "From: <$emailFrom>" . "\r\n";
+  mail($emailTo, $emailSubject, $message, $headers);
+
+  $data['success'] = true;
+  echo json_encode($data);
+}
+}
